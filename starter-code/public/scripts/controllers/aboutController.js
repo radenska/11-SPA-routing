45e2757b85f4187559ad5e705e540214ff5966ee
radenska <yana_radenska@yahoo.com>
2017-01-17 13:34:16 -0800

    did TODOs in routes, articleController and aboutController

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,7 +3,12 @@
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = function () {
+    $('main').hide();
+    $('#about').show();
+  }
+
 
   module.aboutController = aboutController;
 })(window);
